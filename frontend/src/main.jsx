ad0d150a294ef5df49ddad66b5a847030161a958
Zhe Xuan <94375191+Delocy@users.noreply.github.com>
2026-09-22 10:43:42 +0800

    redesign frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/800.css";
 import App from "./App.jsx";
 import "./styles.css";
 
